834b9ca3cfc46cbc2f5ac28d3018c42f42d55c6f
shaheen16 <shaheen16@shaheen16-VirtualBox.(none)>
2013-11-17 01:04:47 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mapper::Application.config.secret_key_base = '6c399d7f651c1a66aa729ca740ca53240474f7f99dda558791aa312d6b37d7ed8e8fd5a96c627aa6907dcb89161556ae0f743b455e8e200b7549011d7c0d71af'
