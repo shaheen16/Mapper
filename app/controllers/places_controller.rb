5ad0b933f36e5f457b741cd51104998e3bc0aa01
shaheen16 <shaheen16@shaheen16-VirtualBox.(none)>
2013-11-17 02:26:44 -0500

    add form for the place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,4 +2,20 @@ class PlacesController < ApplicationController
 	def index
 		@place = Place.last
 	end
+
+	def new
+		@place = Place.new
+	end
+
+	def create 
+		@place = Place.create(place_params)
+		redirect_to root_path
 end
+end
+
+private
+
+def place_params
+	params.require(:place).permit(:title, :address)
+	end
+
